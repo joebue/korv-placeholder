76145e1a8eab3b2f5d54d6973a2004cf7d35c0f0
Joerg Buesing <joebues@gmail.com>
2024-09-03 20:07:44 +0200

    text color

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,7 @@
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                     <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
                         <div class="flex lg:justify-center lg:col-start-2">
-                            <p class="dark:text-white  text-4xl font-bold">heikos lilla korvffabrik comming soon ....</p>
+                            <p class="dark:text-white  text-4xl font-bold">heikos lilla korvfabrik comming soon ....</p>
 
                         </div>
 
